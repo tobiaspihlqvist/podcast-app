a87d43c841806c94061cebf7db58d4f4604f6e9e
Tobias <tubbeh_97@hotmail.com>
2018-10-23 22:02:30 +0200

    Lagt till generisk CustomList, samt interface IListable. Påbörjade kategori listan men kan inte använda metoden ListViewItem()

---
+++ Podcast/Podcast/Business logic layer/Category.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Podcast.Business_logic_layer
+{
+    public class Category : IListable
+    {
+        string Name { get; set; }
+        CustomList<String> CategoryList { get; set; }
+
+        public Category(){
+            CustomList<String> CategoryList = new CustomList<String>();
+            CategoryList.Add("Humour");
+            CategoryList.Add("Horror");
+            CategoryList.Add("Business");
+            CategoryList.Add("Lifestyle");
+        }
+
+        public virtual ListViewItem ToListViewItem()
+        {
+            return new ListViewItem(CategoryList);
+        }
+    }
+}
---
+++ Podcast/Podcast/Business logic layer/CustomList.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Podcast.Business_logic_layer
+{
+    public class CustomList<T>     
+    {
+        private List<T> List { get; set; }
+
+        public CustomList()
+        {
+            List = new List<T>();
+        }
+
+        public void Add(T item)
+        {
+            List.Add(item);
+        }
+        public T Get(int index)
+        {
+            return List[index];
+        }
+    }
+}
